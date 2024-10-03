61cd2b9dad1f0470427c236fcb50281c42b81eac
ManikantaKSN <manikantaksn28@gmail.com>
2024-10-03 16:41:15 +0530

    Minor change in app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const multer = require('multer'); 
 const Event = require('./models/Event');
